18cf2b444338d1e3fc4666905e0e4bae2776c605
kaue_sv <kauesousavieira534@gmail.com>
2024-07-17 13:01:10 -0300

    adicionando ponto e vingulas e criação do arquivo js dos professores

--- js/app.js
+++ js/app.js
@@ -3,24 +3,24 @@ function comprar(){
     let quantidade = document.getElementById("qtd").value;
 
     //Subtração dos ingressos disponíveis
-    let novaQuantidadeIngressos = subtracaoIngressos(tipoIngresso, quantidade)
+    let novaQuantidadeIngressos = subtracaoIngressos(tipoIngresso, quantidade);
 
     //  Evitar compra sem cadeiras
     if(novaQuantidadeIngressos < 0){
-        alert(`quantidade indisponível para ${tipoIngresso}`)
+        alert(`quantidade indisponível para ${tipoIngresso}`);
         return NaN
     }
 
     //Altera a quantidade de ingressos
-    alteraQuantidadeIngressos(tipoIngresso, novaQuantidadeIngressos)
+    alteraQuantidadeIngressos(tipoIngresso, novaQuantidadeIngressos);
 }
 
 function subtracaoIngressos(tipo, quantidadeComprada){
     let quantidadeIngressos = document.getElementById(`qtd-${tipo}`).textContent;
-    return quantidadeIngressos - quantidadeComprada
+    return quantidadeIngressos - quantidadeComprada;
 }
 
 function alteraQuantidadeIngressos(tipo, quantidade){
     const qtdTipo = document.getElementById(`qtd-${tipo}`);
-    qtdTipo.innerHTML = `<span id="qtd-${tipo}">${quantidade}</span>`
+    qtdTipo.innerHTML = `<span id="qtd-${tipo}">${quantidade}</span>`;
 }
